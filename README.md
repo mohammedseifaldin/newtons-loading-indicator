ff9e6669ad1d0713fe19a485503e3af6d234ded2
SAIF <mohammedseifaldin@gmail.com>
2023-12-09 21:37:44 +0300

    initial release done

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-Loading Animation resembles Newton's cradle to use with education apps
+A Loading Animation resembles Newton's cradle ,it's beautiful to use with education category apps.
 
-Features
-very responsive and looks good in every screen
-very customizable and has many properties
-very simple to use
+## Features
+
+- ⭐ very responsive and looks good in every screen
+- ⭐ very customizable and has many properties
+- ⭐ very simple to use
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/hivedb/hive/master/.github/logo_transparent.svg?sanitize=true" width="350px">
+</p>
+
+
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.purpleAccent,
-          brightness: Brightness.dark,
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -23,22 +20,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Example"),
-      ),
-      body: const NewtonLoadingIndicator(),
+    return const Scaffold(
+      body: NewtonLoadingIndicator(),
     );
   }
 }
